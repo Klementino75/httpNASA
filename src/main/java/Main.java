c30a4e60b4641ed840c12ef6d8c2357cec5b0df2
Klementino75 <klementino75@ya.ru>
2024-09-24 19:05:04 +0300

    Added by README.md

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public class Main {
+    public static final String URL_PIC =
+            "https://api.nasa.gov/planetary/apod?api_key=qWtbF7dIk9GeW4bMWrOBvemSArNx1fDxjmSYMqtl";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        
+    }
+}
